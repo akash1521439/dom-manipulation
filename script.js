6e43574ef640971b2673eef0fc1bf43f91fae26c
Mahbubur Rahman Akash <1521439@iub.edu.bd>
2018-06-01 19:34:53 +0000

    task 1 2 3

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 /* Task 1 */
-
+ 
+var ta1 = document.getElementById('heading');
+console.log(ta1.innerText);
 
 /* Task 2 using plain JavaScript */
+function addTexts(){
+    var task2div = document.getElementById('task2a');
+    var para = document.createElement('p');
+    para.innerText="Hello World!";
+    task2div.appendChild(para);
+}
 
+function changeColor(newColor) {
+  var color = document.getElementsByTagName('body')[0];
+  color.style.backgroundColor = newColor;
+}
+
+
+function Redirect(){
+    window.location.href = "https://www.google.com/";
+    
+}
+var Google = document.getElementById('Google');
+Google.addEventListener('click',Redirect);
 
 
 /* Task 4 using jQuery */
